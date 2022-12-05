c2377e855c300b97a026a5f7314f3921087a2901
Dev.NikuRamen <dev.alter0@gmail.com>
2022-12-05 11:14:34 +0900

    Lazy instance 적용

---
+++ UnitySingleton/Singelton.cs
@@ -0,0 +1,31 @@
+﻿namespace UnitySingleton
+{
+    using UnityEngine;
+
+
+#nullable enable
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static readonly Lazy<T> _instance =
+            new Lazy<T>(() =>
+            {
+                T instance = FindObjectOfType(typeof(T)) as T;
+
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject("GameManagers");
+                    instance = obj.AddComponent(typeof(T)) as T;
+                }
+
+                return instance;
+            });
+
+        public static T Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+    }
+}
